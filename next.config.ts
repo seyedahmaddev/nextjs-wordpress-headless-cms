096718db3bcfff1558e8870aa165690aac689f34
seyedahmad <seyedahmaddv@gmail.com>
2026-07-08 18:08:32 +0330

    modify next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -6,28 +6,16 @@ const wordpressUrl = process.env.WORDPRESS_URL;
 const nextConfig: NextConfig = {
   output: "standalone",
   images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: wordpressHostname, // ← حالا مطمئناً string هست
-        port: "",
-        pathname: "/wp-content/uploads/**",
-      },
-      // این رو هم اضافه کن برای اطمینان
-      {
-        protocol: "https",
-        hostname: "seyedahmaddev.ir",
-        port: "",
-        pathname: "/wp-content/uploads/**",
-      },
-    ],
-    // تنظیمات بهینه‌سازی
-    deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
-    imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
-    dangerouslyAllowSVG: true,
-    contentDispositionType: 'attachment',
-    contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
-    minimumCacheTTL: 60,
+    remotePatterns: wordpressHostname
+      ? [
+          {
+            protocol: "https",
+            hostname: wordpressHostname,
+            port: "",
+            pathname: "/**",
+          },
+        ]
+      : [],
   },
   async redirects() {
     if (!wordpressUrl) {
