af3d859f83dbb9296ad4de1de739c3125e9660b9
Seyed Ahmad Gholami <110930355+seyedahmaddev@users.noreply.github.com>
2026-08-17 07:39:30 +0330

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -5,7 +5,7 @@ type SiteConfig = {
 };
 
 export const siteConfig: SiteConfig = {
-  site_name: "سیداحمد غلامی | توسعه‌دهنده وب طراح سایت و اپلیکیشن"
+  site_name: "سیداحمد غلامی | توسعه‌دهنده وب طراح سایت و اپلیکیشن",
   site_description: "توسعه‌دهنده React & Next.js و متخصص معماری Headless WordPress", 
   site_domain: "https://seyedahmaddev.ir", 
 };
