474b4373bf28869deed72989a5373789852e0b9e
seyedahmad <seyedahmaddv@gmail.com>
2026-09-09 15:56:51 +0330

    change text of home page

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -15,7 +15,7 @@ export default function Hero() {
                 <div className="mx-auto max-w-7xl px-6 py-4 lg:py-40 lg:px-8">
                     <div className="mx-auto max-w-2xl lg:mx-0 lg:grid lg:max-w-none lg:grid-cols-2 lg:gap-x-16 lg:gap-y-8 xl:grid-cols-1 xl:grid-rows-1 xl:gap-x-8">
                         <h1 className="max-w-2xl text-2xl font-semibold tracking-tight text-balance text-gray-900 sm:text-3xl lg:col-span-2 xl:col-auto">
-                           طراحی سایت و توسعه وب سیداحمدغلامی | توسعه‌دهنده React & Next.js
+                          طراحی سایت حرفه‌ای و فروشگاهی با React، Next.js و وردپرس Headless
                         </h1>
                         <div className="mt-6 max-w-xl lg:mt-0 xl:col-end-1 xl:row-start-1">
                             <p className="text-lg font-medium text-pretty text-gray-500 sm:text-xl/8">
@@ -29,10 +29,10 @@ export default function Hero() {
                                     href="/مشاوره-طراحی-سایت"
                                     className="rounded-md bg-blue-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-xs hover:bg-blue-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
                                 >
-                                    همین حالا شروع کنید
+                                    شروع پروژه
                                 </Link>
                                 <Link href="#Techs" className="text-sm/6 font-semibold text-gray-900">
-                                   تکنولوژی ها<span aria-hidden="true">←</span>
+                                   مشاهده خدمات<span aria-hidden="true">←</span>
                                 </Link>
                             </div>
                         </div>
